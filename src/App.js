ff6257d8ee58d020dc37f4fd2b83bc6763480fb6
Victor <azevedovictor6699@gmail.com>
2022-04-11 10:39:02 -0300

    Ola Mundo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" />        
+        <h1>Olá mundo</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
